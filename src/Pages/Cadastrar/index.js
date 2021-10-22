2a2871f7c5f9098ab41ad70ce989e418ca7a4da3
solusnet10 <solussoftware@outlook.com>
2021-10-22 18:21:07 -0300

    Add conexao com a api

--- src/Pages/Cadastrar/index.js
+++ src/Pages/Cadastrar/index.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Titulo } from './style'
 
 export const Cadastrar = () => {
+
+    const [produto, setProduto] = useState({
+        titulo: '',
+        descricao: ''
+    })
+    const valorInput = e => setProduto({...produto, [e.target.name]: e.target.value }) 
+
+    const cadProduto = async e =>  {
+        e.preventDefault()
+        console.log(produto.titulo)
+    }
+
     return (
         <div>
             <Titulo>Cadastrar</Titulo>
 
-            <form>
+            <form onSubmit={cadProduto}>
                 <label>Título: </label>
-                <input type="text" name="titulo" placeholder="Título do produto" /><br /><br />
+                <input type="text" name="titulo" placeholder="Título do produto" onChange={valorInput}/><br /><br />
                 <label>Descrição:</label>
-                <input type="text" name="descricao" placeholder="Descrição do produto" /><br /><br />
-                <button type="button">Cadastrar</button>
+                <input type="text" name="descricao" placeholder="Descrição do produto" onChange={valorInput} /><br /><br />
+                <button type="submit">Cadastrar</button>
             </form>
         </div>
     )
